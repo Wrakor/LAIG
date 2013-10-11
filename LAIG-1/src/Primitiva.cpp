218224be61a94036767798f7dca3ada60eef1869
vascofg <vascofg@gmail.com>
2013-10-11 01:24:27 +0100

    comentários

--- LAIG-1/src/Primitiva.cpp
+++ LAIG-1/src/Primitiva.cpp
@@ -10,6 +10,7 @@ Rectangle::Rectangle(float x1,float x2,float y1,float y2)
 }
 
 void Rectangle::draw(){
+	//faltam normais
 	glBegin(GL_QUADS);
 		glTexCoord2f(0,0);
 		glVertex2f(x1,y1);
@@ -34,7 +35,7 @@ Triangle::Triangle(float x1,float x2,float x3,float y1, float y2, float y3, floa
 void Triangle::draw()
 {
 	glBegin(GL_TRIANGLES);
-		//FALTAM TEXTURAS
+		//FALTAM TEXTURAS e normais
 		glVertex3f(x1, y1, z1);
         glVertex3f(x2, y2, z2);
         glVertex3f(x3, y3, z3);
